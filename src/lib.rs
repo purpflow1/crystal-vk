81b2c55653568f2c1a2e48a8ea74e92bb9a18855
purpflow1 <purpflow1@gmail.com>
2026-02-05 11:21:30 +0400

    Docs update

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
-//! # crystal-vk
-//! crystal-vk is a graphics wrapper around vulkan
+//! ## 🚀 Quick Start
+//! Add `crystal-vk` to your `Cargo.toml` dependencies
+//! ```toml
+//! [dependencies]
+//! crystal-vk = "0.0.4"
+//! ```
+//! Here is a minimal example showing how to create a Vulkan device:
+//! ```rust
+//! use winit::{dpi::LogicalSize, window::Window};
+//! use crystal_vk::device::Device;
+//!
+//! pub fn new(event_loop: &winit::event_loop::ActiveEventLoop) {
+//!     // winit window creation
+//!     let window = {
+//!         event_loop
+//!             .create_window(
+//!                 Window::default_attributes()
+//!                     .with_inner_size(LogicalSize::new(300, 300)),
+//!             )
+//!             .unwrap()
+//!     };
+//!
+//!     // creates logical device from the first element of the list of available devices
+//!     let (device, queues) = Device::with_present(|devices| devices[0].clone(), &window).unwrap();
+//! }
+//! ```
 
 pub mod buffer;
 pub mod command;
