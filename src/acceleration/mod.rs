b626cbd36e5990575370012ca903bb4f00de0f74
purpflow1 <purpflow1@gmail.com>
2026-03-07 18:14:57 +0400

    Update mod.rs

--- src/acceleration/mod.rs
+++ src/acceleration/mod.rs
@@ -94,7 +94,7 @@ impl AccelerationStructure {
         let g = geometries.iter().map(|g| g.as_vk()).collect::<Vec<_>>();
 
         let build_info = vk::AccelerationStructureBuildGeometryInfoKHR::default()
-            .ty(vk::AccelerationStructureTypeKHR::GENERIC)
+            .ty(vk::AccelerationStructureTypeKHR::BOTTOM_LEVEL)
             .flags(vk::BuildAccelerationStructureFlagsKHR::PREFER_FAST_TRACE)
             .geometries(&g);
 
