f35d290b6cc4f2d6937bc70bdbb3dc792e9ef6b0
purpflow1 <purpflow1@gmail.com>
2026-05-28 14:00:26 +0700

    Updated default GPU finding

--- src/instance/mod.rs
+++ src/instance/mod.rs
@@ -59,25 +59,41 @@ impl Instance {
     ) -> Result<Option<PhysicalDevice>, Box<dyn Error>> {
         let physical_devices_raw = unsafe { self.handle.enumerate_physical_devices() }?;
 
-        let is_surface = surface.is_some();
-
         let physical_devices =
             unsafe { PhysicalDevice::new(self.clone(), surface, physical_devices_raw) }?;
 
-        let physical_device = if is_surface {
-            physical_devices.into_iter().find(|pd| {
+        let physical_device = {
+            let mut iter = physical_devices.into_iter();
+
+            // Level 1
+            let mut d = iter.find(|pd| {
                 pd.info
                     .queue_families_info
                     .iter()
                     .any(|f| f.flags.contains(vk::QueueFlags::GRAPHICS) && f.present_support)
-            })
-        } else {
-            physical_devices.into_iter().find(|pd| {
-                pd.info
-                    .queue_families_info
-                    .iter()
-                    .any(|f| f.flags.contains(vk::QueueFlags::COMPUTE))
-            })
+            });
+
+            // Level 2
+            if d.is_none() {
+                d = iter.find(|pd| {
+                    pd.info
+                        .queue_families_info
+                        .iter()
+                        .any(|f| f.flags.contains(vk::QueueFlags::GRAPHICS))
+                });
+
+                // Level 3
+                if d.is_none() {
+                    d = iter.find(|pd| {
+                        pd.info
+                            .queue_families_info
+                            .iter()
+                            .any(|f| f.flags.contains(vk::QueueFlags::COMPUTE))
+                    });
+                }
+            }
+
+            d
         };
 
         Ok(physical_device)
